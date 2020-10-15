55e5e2598adfaf9d58da7de0b16274246a2befb4
wcy <s2288156@163.com>
2020-10-15 14:09:22 +0800

    init ums module

---
+++ ums/src/main/java/com/mall/ums/UmsApplication.java
@@ -0,0 +1,16 @@
+package com.mall.ums;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author wcy
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class UmsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UmsApplication.class, args);
+    }
+}
