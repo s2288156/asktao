7f2dc2261beb241987b4fce80833e6e25508f7a4
wcy <s2288156@163.com>
2020-10-15 14:30:33 +0800

    common-web 增加WebExceptionHandler和LogInterceptor

---
+++ common-web/src/main/java/com/mall/web/config/ControllerExceptionHandler.java
@@ -0,0 +1,81 @@
+package com.mall.web.config;
+
+import com.mall.lib.domain.RestResult;
+import com.mall.lib.ex.BizException;
+import com.mall.lib.ex.ResultCodeEnum;
+import com.mall.lib.ex.SysException;
+import feign.FeignException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+
+/**
+ * @author wcy
+ */
+@Slf4j
+@RestControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(BizException.class)
+    public ResponseEntity<?> handleBizException(BizException ex) {
+        log.warn("[BizException]: {}", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(RestResult.error(ex.getCode(), ex.getMessage()));
+    }
+
+    @ExceptionHandler(SysException.class)
+    public ResponseEntity<?> handleBizException(SysException ex) {
+        log.error("[SysException]: {}", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(RestResult.error(ex.getCode(), ex.getMessage()));
+    }
+
+    @ExceptionHandler(value = BindException.class)
+    public ResponseEntity<?> bingEx(BindException e) {
+        log.warn("校验异常字段filedName: {}", e.getFieldError().getField());
+        return ResponseEntity.badRequest().body(RestResult.error(ResultCodeEnum.USER_ERROR.code(),
+                e.getFieldError().getDefaultMessage()));
+    }
+
+//    @ExceptionHandler(AccessDeniedException.class)
+//    public ResponseEntity<?> handleAccessDeniedException(AccessDeniedException ex) {
+//        log.warn("[AccessDeniedException]: {}", ex.getMessage());
+//        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(RestResult.error(ResultCodeEnum.PERMISSION_ERROR));
+//    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleException(Exception ex) {
+        log.error("[UnknownException]: ", ex);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(RestResult.error(ResultCodeEnum.SYS_EXECUTE_ERROR));
+    }
+
+    @ExceptionHandler(FeignException.class)
+    public ResponseEntity<?> handleFeignException(FeignException ex) {
+        log.error("feign调用异常: {}", ex.getMessage());
+        if (ex.responseBody().isPresent()) {
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(new String(ex.responseBody().get().array()));
+        }
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(RestResult.error(ResultCodeEnum.SYS_EXECUTE_ERROR));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleParamNotValidException(MethodArgumentNotValidException ex) {
+        BindingResult bindingResult = ex.getBindingResult();
+        List<ObjectError> allErrors = bindingResult.getAllErrors();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(RestResult.error(ResultCodeEnum.USER_ERROR.code(), allErrors.get(0).getDefaultMessage()));
+    }
+
+}
---
+++ common-web/src/main/java/com/mall/web/config/LogInterceptor.java
@@ -0,0 +1,79 @@
+package com.mall.web.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+/**
+ * @author wcy
+ */
+@Component
+@Slf4j
+public class LogInterceptor implements HandlerInterceptor {
+
+    private ThreadLocal<StopWatch> swThread = new ThreadLocal<>();
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        StopWatch sw = new StopWatch();
+        swThread.set(sw);
+        sw.start();
+        MDC.clear();
+        MDC.put("threadId", UUID.randomUUID().toString().replaceAll("-", ""));
+
+        log.info("请求路径: {}, Authorization = {}", request.getRequestURI(), request.getHeader("Authorization"));
+        requestDebugLog(request);
+        return true;
+    }
+
+    private void requestDebugLog(HttpServletRequest request) {
+        if (log.isDebugEnabled()) {
+            // request body 参数获取
+            Map<String, String> body = new HashMap<>(16);
+            Enumeration<String> parameterNames = request.getParameterNames();
+            while (parameterNames.hasMoreElements()) {
+                String name = parameterNames.nextElement();
+                body.put(name, request.getParameter(name));
+            }
+            log.info("body: {}", body);
+        }
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) {
+        swThread.get().stop();
+        swThread.get().start();
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
+        StopWatch sw = swThread.get();
+        sw.stop();
+        String method = handler.getClass().getSimpleName();
+        if (handler instanceof HandlerMethod) {
+            String beanType = ((HandlerMethod) handler).getBeanType().getName();
+            String methodName = ((HandlerMethod) handler).getMethod().getName();
+            method = beanType + "." + methodName;
+        }
+
+        log.info("METHOD = {}; STATUS = {}; EX = {}; 总时间 = {}ms; 完成时间 = {}ms; 前置时间 = {}ms",
+                method,
+                response.getStatus(),
+                ex == null ? "-" : ex.getClass().getSimpleName(),
+                sw.getTotalTimeMillis(),
+                sw.getTotalTimeMillis() - sw.getLastTaskTimeMillis(),
+                sw.getLastTaskTimeMillis());
+        swThread.remove();
+    }
+}
