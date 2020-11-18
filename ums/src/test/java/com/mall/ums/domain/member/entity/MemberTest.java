99419bd50da5e753e8411c78fec446b90282b118
wcy <s2288156@163.com>
2020-11-18 11:36:30 +0800

    feat(ums): Member领域register()单元测试

--- ums/src/test/java/com/mall/ums/domain/member/entity/MemberTest.java
+++ ums/src/test/java/com/mall/ums/domain/member/entity/MemberTest.java
@@ -1,9 +1,13 @@
 package com.mall.ums.domain.member.entity;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.mall.ums.BaseTest;
+import com.mall.ums.infrastructure.dataobject.UserDO;
+import com.mall.ums.infrastructure.mapper.UserMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,7 +19,11 @@ class MemberTest extends BaseTest {
     @Autowired
     private Member member;
 
+    @Autowired
+    private UserMapper userMapper;
+
     @Test
+    @Transactional
     void testRegister() {
         LoginInfo loginInfo = new LoginInfo();
         loginInfo.setUsername("laozhang");
@@ -23,5 +31,9 @@ class MemberTest extends BaseTest {
 
         member.setLoginInfo(loginInfo);
         member.register();
+
+        UserDO userDO = userMapper.selectOne(new LambdaQueryWrapper<UserDO>().eq(UserDO::getUsername, loginInfo.getUsername()));
+        assertNotNull(userDO);
+        log.warn("userDO = {}", userDO);
     }
 }
