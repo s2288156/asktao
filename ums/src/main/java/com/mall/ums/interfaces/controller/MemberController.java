66fb0e709b124dbdf0a6a27406969dd17c898c2b
wcy <s2288156@163.com>
2020-11-20 15:24:49 +0800

    feat(ums): Dockerfile

--- ums/src/main/java/com/mall/ums/interfaces/controller/MemberController.java
+++ ums/src/main/java/com/mall/ums/interfaces/controller/MemberController.java
@@ -4,6 +4,7 @@ import com.mall.lib.domain.RestResult;
 import com.mall.ums.application.service.IAccountService;
 import com.mall.ums.interfaces.form.MemberRegisterForm;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +20,7 @@ public class MemberController {
     private IAccountService accountService;
 
     @PostMapping("/register")
-    public RestResult<?> register(MemberRegisterForm memberRegister) {
+    public RestResult<?> register(@Validated MemberRegisterForm memberRegister) {
         accountService.registerMember(memberRegister.convert2Dto());
         return RestResult.success();
     }
