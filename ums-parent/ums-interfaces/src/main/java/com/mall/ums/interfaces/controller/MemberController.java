56321b4626b8968f9c2c04a157ceb366399596e3
wcy <s2288156@163.com>
2020-12-16 14:17:38 +0800

    feat(ums): 会员登录接口返回值统一包装类

--- ums-parent/ums-interfaces/src/main/java/com/mall/ums/interfaces/controller/MemberController.java
+++ ums-parent/ums-interfaces/src/main/java/com/mall/ums/interfaces/controller/MemberController.java
@@ -1,13 +1,12 @@
 package com.mall.ums.interfaces.controller;
 
 import com.mall.lib.domain.RestResponse;
+import com.mall.ums.application.dto.MemberLoginCmd;
 import com.mall.ums.application.service.IAccountService;
 import com.mall.ums.domain.member.entity.Member;
-import com.mall.ums.application.dto.MemberLoginCmd;
 import com.mall.ums.interfaces.form.MemberRegisterForm;
 import com.mall.ums.interfaces.vo.MemberDetailVO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -34,7 +33,7 @@ public class MemberController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@Validated MemberLoginCmd loginCmd) {
-        return accountService.login(loginCmd);
+    public RestResponse<?> login(@Validated MemberLoginCmd loginCmd) {
+        return RestResponse.success(accountService.login(loginCmd).getBody());
     }
 }
