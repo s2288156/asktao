744412cd4a909a2663ee63dd77c42e838b168302
wcy <s2288156@163.com>
2021-02-05 15:07:24 +0800

    更新nacos地址为node0.com；更新数据库地址为node0.com；更新初始脚本；

--- ums-parent/ums-interfaces/src/main/resources/db/migration/1.0.x/V1.0.1__init_data_role.sql
+++ ums-parent/ums-interfaces/src/main/resources/db/migration/1.0.x/V1.0.1__init_data_role.sql
@@ -4,9 +4,9 @@ INSERT INTO t_admin_role (id, admin_id, role_id)
 VALUES ('2', '1', '2');
 
 INSERT INTO t_admin (id, create_time, update_time, name, age, gender, status, birthday, icon, username, password, mobile, email)
-VALUES ('1', '2021-01-08 14:47:52', '2021-01-08 14:49:07', null, null, null, 1, null, null, 'admin', '$2a$10$xL/O7FotMed54uKC/XOGpurYSalY6PE2ZuX8chBdnML0TAgXZ6/Tq', null, null);
+VALUES ('1', now(), now(), null, null, null, 1, null, 'https://wcy-img.oss-cn-beijing.aliyuncs.com/images/avatar/default_avatar.jpg', 'admin', '$2a$10$xL/O7FotMed54uKC/XOGpurYSalY6PE2ZuX8chBdnML0TAgXZ6/Tq', null, null);
 
 INSERT INTO t_role (id, create_time, update_time, name, description, sort)
-VALUES ('1', '2021-01-08 14:13:57', '2021-01-08 14:13:57', 'ROOT', '超级管理员', 0);
+VALUES ('1', now(), now(), 'ROOT', '超级管理员', 0);
 INSERT INTO t_role (id, create_time, update_time, name, description, sort)
-VALUES ('2', '2021-01-08 14:13:57', '2021-01-08 14:13:57', 'GUEST', '访客', 1);
+VALUES ('2', now(), now(), 'GUEST', '访客', 1);
