04344e063ab29e55e2aab471fc0450e221361add
武春阳 <s2288156@163.com>
2021-08-06 17:12:55 +0800

    init security module

--- commons-parent/common-mybatis/src/main/java/com/asktao/mybatis/base/ColumnName.java
+++ commons-parent/common-mybatis/src/main/java/com/asktao/mybatis/base/ColumnName.java
@@ -1,5 +1,8 @@
 package com.asktao.mybatis.base;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author wcy
  */
@@ -10,4 +13,12 @@ public class ColumnName {
     public static final String CREATE_TIME = "create_time";
 
     public static final String UPDATE_TIME = "update_time";
+
+    public static void main(String[] args) {
+        List<Integer> asList = Arrays.asList(1, 2, 3);
+        System.out.println(asList.get(0));
+        System.out.println(asList.get(1));
+        System.out.println(asList.get(2));
+        asList.add(4);
+    }
 }
