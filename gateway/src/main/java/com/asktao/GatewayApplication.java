71d36e20fddff354b4cf1b7a55a7de7c8bf72fa1
武春阳 <s2288156@163.com>
2021-09-02 14:03:58 +0800

    gateway init

---
+++ gateway/src/main/java/com/asktao/GatewayApplication.java
@@ -0,0 +1,14 @@
+package com.asktao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wuyang
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
