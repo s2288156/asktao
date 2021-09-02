1831d4c5bcf5ff9ee442fe13f82eb13332ec44b5
武春阳 <s2288156@163.com>
2021-09-02 14:19:03 +0800

    mall controller module init

---
+++ commons-parent/common-web/src/main/java/com/asktao/web/config/CoreWebConfig.java
@@ -0,0 +1,20 @@
+package com.asktao.web.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author wuyang
+ */
+@Configuration
+public class CoreWebConfig implements WebMvcConfigurer {
+    @Autowired
+    private LogInterceptor logInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(logInterceptor).addPathPatterns("/**");
+    }
+}
---
+++ mall/mall-controller/src/main/java/com/asktao/MallApplication.java
@@ -0,0 +1,14 @@
+package com.asktao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wuyang
+ */
+@SpringBootApplication
+public class MallApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MallApplication.class, args);
+    }
+}
