3749e75ad2df6518f299650e1f4e02d03c9b5242
武春阳 <s2288156@163.com>
2021-09-14 14:37:51 +0800

    pms 数据库配置

---
+++ pms/pms-start/src/main/java/com/asktao/PmsApplication.java
@@ -0,0 +1,14 @@
+package com.asktao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wuyang
+ */
+@SpringBootApplication
+public class PmsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PmsApplication.class, args);
+    }
+}
