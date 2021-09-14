eaebb93048cd4e9fbe5a001864098bca31c320d5
武春阳 <s2288156@163.com>
2021-09-14 15:20:15 +0800

    oms配置

---
+++ oms/oms-start/src/main/java/com/asktao/OmsApplication.java
@@ -0,0 +1,14 @@
+package com.asktao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wuyang
+ */
+@SpringBootApplication
+public class OmsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OmsApplication.class, args);
+    }
+}
