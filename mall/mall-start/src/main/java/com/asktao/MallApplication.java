f25d138b9cdde620f03bad6bf9d62fb3479122c2
武春阳 <s2288156@163.com>
2021-09-15 14:47:40 +0800

    [gw]: 网关接入注册和配置中心

--- mall/mall-start/src/main/java/com/asktao/MallApplication.java
+++ mall/mall-start/src/main/java/com/asktao/MallApplication.java
@@ -2,10 +2,12 @@ package com.asktao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author wuyang
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MallApplication {
     public static void main(String[] args) {
